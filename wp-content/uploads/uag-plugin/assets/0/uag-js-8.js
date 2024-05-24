ed49600fca5c366467e92c4bb7eb32430c6bb48e
zaimsite <websitebaru13@gmail.com>
2024-05-24 11:53:01 +0700

    webspanemu

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-8.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-452a68f6' );
+});
+ });
---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-1496.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-a31d33dc' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-1395e03c' );
+});
+ });
